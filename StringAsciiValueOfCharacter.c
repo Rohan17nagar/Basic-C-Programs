2d0c70678e15619d01aa42cc21cc2884fd59c94c
Rohan Nagar <46292211+Rohan17nagar@users.noreply.github.com>
2020-02-14 14:24:45 +0530

    Create StringAsciiValueOfCharacter.c

---
+++ StringAsciiValueOfCharacter.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+Ascii value of a character
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter the character values:\n");
+       scanf("%c",&c);
+    printf("%d",c);
+    return 0;
+}
