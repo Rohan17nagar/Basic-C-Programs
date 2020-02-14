9bfcf82315dcec3744dd91904e199615409a5205
Rohan Nagar <46292211+Rohan17nagar@users.noreply.github.com>
2020-02-14 16:28:28 +0530

    Create CheckPallindrome.c

---
+++ CheckPallindrome.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+* Pallindrome number or not..
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{   
+    int num;
+    int temp,count=0;
+    scanf("%d",&num);
+    temp = num;
+    while(temp!=0){
+        temp = temp/10;
+        count++;
+    }
+    for(i=0;i<)
+    return 0;
+}
